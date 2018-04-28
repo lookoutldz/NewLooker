e7da6faeb5287966c941f58e60adf1a7c68988b8
looko <lookoutldz@outlook.com>
2018-04-28 22:54:47 +0800

    a little

--- src/main/java/looko/looker/release/controller/ProfileController.java
+++ src/main/java/looko/looker/release/controller/ProfileController.java
@@ -1,16 +1,29 @@
 package looko.looker.release.controller;
 
+import looko.looker.release.service.DB_OwnedGameService;
+import looko.looker.release.service.DB_PlayerAchiService;
+import looko.looker.release.service.DB_PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProfileController {
 
+    @Autowired
+    DB_PlayerService playerService;
+    @Autowired
+    DB_OwnedGameService ownedGameService;
+    @Autowired
+    DB_PlayerAchiService achiService;
+
     @RequestMapping("/profile")
-    public String profilePage(@ModelAttribute("steamid") String steamid){
-        if (steamid != null)
-            System.out.printf("steamid=" + steamid + "\n");
+    public String profilePage(@ModelAttribute("steamid") String steamid, ModelAndView modelAndView){
+        if (steamid == null)
+            return "index";
+
 
         return "profile";
     }
